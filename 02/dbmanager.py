9d1e5699ababcb8f03f34e6055a9ac88156b195d
ZhuQinxiang <zhuqinxiang516@gmail.com>
2017-03-08 17:21:57 +0800

    fix bug: cannot insert data to database (in 02/dbmanager)

--- 02/dbmanager.py
+++ 02/dbmanager.py
@@ -17,7 +17,7 @@ class CrawlDatabaseManager:
         "CREATE TABLE `urls` ("
         "  `index` int(11) NOT NULL AUTO_INCREMENT," # index of queue
         "  `url` varchar(512) NOT NULL,"
-        "  `md5` varchar(16) NOT NULL,"
+        "  `md5` varchar(32) NOT NULL,"
         "  `status` varchar(11) NOT NULL DEFAULT 'new'," # could be new, downloading and finish
         "  `depth` int(11) NOT NULL,"
         "  `queue_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,"
